38dcb0c2e35a66f373d08cab48195f0b1e219e77
Özcan Kara <ozcankara810@gmail.com>
2023-06-26 00:28:27 +0300

    some changes

---
+++ Homework/2.sql
@@ -0,0 +1,40 @@
+/* 02/6/2023 Odev Soruları: Veritabanlarına ve SQL'e Giriş */
+
+
+1-employees tablosunda çalışanların adını, soyadını ve tam adını ("first_name
+last_name" formatında) listele
+
+2- employees tablosunda "last_name" sütununda boşluk karakteri olan çalışanların
+sayısını bulun (Ipucu – Count ve INSRT fonksiyonlarini kullanin)
+
+3-employees tablosundaki "phone_number" sütunundaki tüm numaraların başına 1
+tane "0" ekleyin (Ipucu Length ve LPAD fonksiyonlarini kullanin)
+4-employees tablosundaki "email" sütununda geçen "gmail com" ifadesini
+"yahoo com" ile değiştirin
+
+5-employees tablosundaki "email" sütununa her email icin "@gmail com"
+ifadesini ekleyiniz
+6-employees tablosundaki "hire_date" sütununda yer alan tarihleri "DD-MON-YY"
+formatında listeleyin
+
+7-employees tablosundaki "first_name" sütunundaki ilk üç karakteri alın
+
+8-employees tablosunda yer alan "job_id" sütununu ters çevirin
+9-employees tablosunda "job_id" sütununda "MAN" kelimesini içeren kayıtları
+listeleyin
+
+10-employees tablosundaki "last_name" sütunundaki harflerin büyük harfe
+dönüştürülmüş halini listeleyin
+11- employees tablosundaki çalışanların işe alındığı tarihten itibaren geçen ay sayısını
+bulun
+12- employees tablosundaki çalışanların işe alındığı tarihten itibaren geçen gün sayısını
+bulun
+13-employees tablosundaki çalışanların doğum tarihlerine göre yaşlarını bulun
+14-employees tablosundaki çalışanların doğum tarihlerini ve haftanın hangi gününde
+doğduklarını bulun
+
+15- employees tablosundaki çalışanların maaşlarının ortalama değerini bulun
+16-employees tablosundaki çalışanların maaşlarının 2 üzeri alın
+17- DIKKAT: Bu sorguyu backup aldiginiz bir tablo uzerinden calistirin Soru:
+Yarattiginiz yeni tablo olan (CTAS ile) "employees_yedek" tablosunda yer alan JOB_ID=
+IT_PROG çalışanların maaşlarını 500 artirin (Ipucu Update statement)
---
+++ Homework/5.sql
@@ -0,0 +1,41 @@
+/*14/6/2023   Odev Soruları: Veritabanlarına ve SQL'e Giriş   */
+
+1-Departments tablosundaki her departmama ait çalışan
+sayısını ve ortalama maaşlarını with Common Table Expression
+ile bulun. Buldugunuz bu sonuctan, her departmanin ismini ve
+calışan sayısıni gosterin.
+Calisan sayisi 0 olan departmanlar için "Calisan Bulunamadı"
+ifadesini kullanın.
+( Ipucu With CTE ve CASE WHEN Ifadelerini Kullanarak
+Yaziniz)
+
+
+2-Çalışanların adlarını ve yöneticilerinin adlarını
+listeleyin. Yöneticisi olmayan çalışanlar için "Yönetici
+Bulunamadı" ifadesini kullanın.
+( Ipucu With CTE ve CASE WHEN Ifadelerini
+Kullanarak Yaziniz)
+
+
+3-HR şemasındaki DEPARTMENTS tablosundaki departmanların
+IDlerini ve o departmanda çalışan toplam çalışan sayısını
+listeleyen bir CTE oluşturun.
+Ek olarak , yeni bir CTE de ise çalışan sayısı
+10'dan fazla olan departmanlar için 'Büyük Departman', 5-10
+arasında olan departmanlar için 'Orta Büyüklükte Departman' ve
+5'ten az olan departmanlar için 'Küçük Departman' olarak bir
+etiket belirtin.
+Son olarak department_id, toplam_calisan_sayisi,
+departman_etiketi kolonlar ile sonucu yukarida en son
+olusturdugunuz CTE den okuyun
+
+
+4- Bir çalışanın maaşinin calistigi departmanin ortalamsindan
+ne kadar fazla oldugunu donduren bir fonksiyon yaziniz.
+Bu fonksiyon, employee ID yi input olarak alir, maasini
+departman ortalamasindan cikarip numerik bir deger dondurur.
+- Yarattiginiz bu fonksiyonu bir select
+querysi icinde cagirip employees tablosundaki her calisan icin
+employee_id, isim, soyisim, maas ve buldugunuz maas farki
+degerini gosteriniz.
+
