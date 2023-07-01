39d59d6e3c28e559aa3c250aa6048bc4e62246b8
Özcan Kara <ozcankara810@gmail.com>
2023-07-01 04:21:50 +0300

    some changes

--- Homework/2.sql
+++ Homework/2.sql
@@ -9,12 +9,12 @@ last_name" formatında) listele
 2- employees tablosunda "last_name" sütununda boşluk karakteri olan çalışanların
 sayısını bulun (Ipucu – Count ve INSRT fonksiyonlarini kullanin)
 
-=>
+=> *** select  count(last_name) as sonuc from employees 
 
 3-employees tablosundaki "phone_number" sütunundaki tüm numaraların başına 1
 tane "0" ekleyin (Ipucu Length ve LPAD fonksiyonlarini kullanin)
 
-=>
+=> select   phone_number ,lpad(phone_number,13,'0')  from employees 
 
 4-employees tablosundaki "email" sütununda geçen "gmail com" ifadesini
 "yahoo com" ile değiştirin
@@ -34,22 +34,36 @@ formatında listeleyin
 
 7-employees tablosundaki "first_name" sütunundaki ilk üç karakteri alın
 
+=> select  first_name ,substr( first_name,1,3) from employees 
+
 8-employees tablosunda yer alan "job_id" sütununu ters çevirin
-=>
+
+=> select  reverse( job_id  ) from employees 
+
+
 9-employees tablosunda "job_id" sütununda "MAN" kelimesini içeren kayıtları
 listeleyin
+
 =>
 
 10-employees tablosundaki "last_name" sütunundaki harflerin büyük harfe
 dönüştürülmüş halini listeleyin
-=>
+
+=> select  upper(last_name)  as sonuc_buyuk  from employees 
+=>select  lower(last_name)  as sonuc_kucuk  from employees 
+
+
 11- employees tablosundaki çalışanların işe alındığı tarihten itibaren geçen ay sayısını
 bulun
+
 =>
 
 12- employees tablosundaki çalışanların işe alındığı tarihten itibaren geçen gün sayısını
 bulun
+
 =>
+
+
 13-employees tablosundaki çalışanların doğum tarihlerine göre yaşlarını bulun
 
 =>
@@ -60,9 +74,14 @@ doğduklarını bulun
 =>
 
 15- employees tablosundaki çalışanların maaşlarının ortalama değerini bulun
-=>
+
+=> select round(avg(salary)) as ortalama_maas from employees 
+
+
 16-employees tablosundaki çalışanların maaşlarının 2 üzeri alın
-=>
+
+=> select salary, power(salary,2) as yeni_maas from  employees 
+
 
 17- DIKKAT: Bu sorguyu backup aldiginiz bir tablo uzerinden calistirin Soru:
 Yarattiginiz yeni tablo olan (CTAS ile) "employees_yedek" tablosunda yer alan JOB_ID=
