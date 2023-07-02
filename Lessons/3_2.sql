4077c1c54a51cf2d5c2da805686b0a5826eb81c0
Özcan Kara <ozcankara810@gmail.com>
2023-07-02 12:47:20 +0300

    some changes

--- Lessons/3_2.sql
+++ Lessons/3_2.sql
@@ -1,4 +1,4 @@
- Cross Join / JOINLER : Database tablolari ile iliskilendirme verileri,tablolari birlestirme.
+ Cross Join / JOINLER : Database tablolari ile iliskilendirme verileri,tablolari birlestirme.Kartezyan carpimi.
 -------------------------------------------------------------------------------
 
    select * from employees e
@@ -25,6 +25,65 @@
 4-FULL OUTER JOIN : Kayit cogaltir.
 
 -- From dan sonra ilk tablo Left Join
+
+Join Ornekleri :
+CREATE TABLE basket_a (
+ a INT PRIMARY KEY,
+ fruit_a VARCHAR (100) NOT NULL
+);
+CREATE TABLE basket_b (
+ b INT PRIMARY KEY,
+ fruit_b VARCHAR (100) NOT NULL
+);
+INSERT INTO basket_a (a, fruit_a)
+VALUES
+ (1, 'Apple'),
+ (2, 'Orange'),
+ (3, 'Banana'),
+ (4, 'Cucumber');
+INSERT INTO basket_b (b, fruit_b)
+VALUES
+ (1, 'Orange'),
+4
+ (2, 'Apple'),
+ (3, 'Watermelon'),
+ (4, 'Pear');
+ 
+select * from basket_a ba 
+select * from basket_b bb 
+ 
+select a,fruit_a, b, fruit_b from basket_a, basket_b -- Cross 
+JOIN
+SELECT
+ a,fruit_a, b, fruit_b
+FROM
+ basket_a
+INNER JOIN basket_b ON fruit_a = fruit_b;
+SELECT
+ a,fruit_a, b, fruit_b
+FROM
+ basket_a
+JOIN basket_b ON fruit_a = fruit_b;
+SELECT
+ a, fruit_a, b, fruit_b
+FROM
+ basket_a
+LEFT JOIN basket_b ON fruit_a = fruit_b;
+SELECT
+ a, fruit_a, b, fruit_b
+FROM
+ basket_a
+RIGHT JOIN basket_b ON fruit_a = fruit_b;
+SELECT
+ a, fruit_a, b, fruit_b
+FROM
+ basket_a
+FULL OUTER JOIN basket_b ON fruit_a = fruit_b;
+
+
+
+
+
 
 
 
