98c40a05d80da7faaa332aabc3987e5d9688d9d6
Özcan Kara <ozcankara810@gmail.com>
2023-07-06 10:02:57 +0300

    some changes

--- Bootcamp SQL/Lessons/5.1.sql
+++ Bootcamp SQL/Lessons/5.1.sql
@@ -1,3 +1,25 @@
 /* 14.06.2023  Konu Anlatim */
 
 --CASE WHEN : IF/ELSE
+
+
+--  Maas araliklarina gore sonuclari goruntuleme:
+---------------------------------------------------
+
+select employee_id ,first_name ,last_name,salary ,
+
+case        -->  Case : Sartlari Olusturma 
+
+	when salary > 5000 and salary < 7000  then 'Dusuk Gelir'
+	when salary > 7001 and salary <= 9000 then 'Ortalama Gelir'
+	when salary > 9000 then 'Yuksek Gelir'
+	else 'Gelir Araligi Bilinmiyor'
+
+end as gelir_araligi                                                    
+
+from employees e 
+
+------------------------------------------------------------------------------------------------------
+
+
+
