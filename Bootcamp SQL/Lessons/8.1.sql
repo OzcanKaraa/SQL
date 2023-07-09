5779dd3642f0576de5cb2cfa38560551f24e454b
Özcan Kara <ozcankara810@gmail.com>
2023-07-10 00:58:57 +0300

    some changes

--- Bootcamp SQL/Lessons/8.1.sql
+++ Bootcamp SQL/Lessons/8.1.sql
@@ -6,12 +6,15 @@ Database uzerinde performans artirma.
 Veri milyonlarca milyarlarca ciktigi zaman veriyi hizlica dondurebilme.
 Sutunlar uzerinden siralama
 Index ile query hizli donus saglar
-scan (tarama islemi )eder.
+scan (tarama islemi) eder.
 Index maliyeti dusurur.
+Index ile sql liste obje tablo ile hizli sonuc alinir.
+Index uzerinde aktif sekilde hizlica search yapip hizli sonuc donduren query
 
-
-
-
+Dezavantaj :
+--------------
+Obje oldugu icin diskte yer kaplar.
+Update insert islemleri yavaslatir.
 
 
 
@@ -41,3 +44,17 @@ where last_name like '%T%'
 --         Filter: ((last_name)::text ~~ '%T%'::text)
 --   ->  Hash  (cost=1.27..1.27 rows=27 width=90)
 --        ->  Seq Scan on departments d  (cost=0.00..1.27 rows=27 width=90)
+
+
+
+--INDEX OLUSTURMA :
+create index emp_lst_name on employees (last_name)
+
+
+
+
+explain
+select e.*,e2.*,department_name from employees e 
+left join departments d on d.department_id = e.department_id
+inner join emp_2 e2 on e2.last_name = e.last_name
+where e.last_name like '%T%'
