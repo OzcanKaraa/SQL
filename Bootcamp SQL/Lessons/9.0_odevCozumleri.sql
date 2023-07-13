f2d78ce28224260e5a4d2f0359c9e042c7b8d5dc
Özcan Kara <ozcankara810@gmail.com>
2023-07-13 06:45:21 +0300

    some changes

---
+++ Bootcamp SQL/Lessons/9.0_odevCozumleri.sql
@@ -0,0 +1,5 @@
+/* 05.07.2023 Odev Cozumleri*/
+
+1-Belirli bir yöneticinin altýnda çalýþan çalýþanlarýn sayýsýný döndüren bir SQL fonksiyonu oluþturun. 
+  Fonksiyonun girdisi yönetici id'si,ciktisi ise integer bir deger olmalýdýr.
+
