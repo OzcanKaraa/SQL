e2a6a66fc581559afcc65ae506b7ca619979d584
Özcan Kara <ozcankara810@gmail.com>
2023-07-13 19:55:09 +0300

    some changes

--- Bootcamp SQL/Lessons/10.0.sql
+++ Bootcamp SQL/Lessons/10.0.sql
@@ -1,6 +1,6 @@
 --12.07.2023 
 
---Triggerlar (Tetikleme)
+--Triggerlar (Tetikleme) -1
 
 Zamana bagli mesela insert etmeden once yapilmasi gerekeni yap. 
 Bir islemi yaparken baska bir islem trigger ile event tetikleniyor.
@@ -10,6 +10,11 @@ Dezavantajlari : Performansi dusurmesi.
 Performansin sorun olmadigi usecaselerde avantajli.
 
 Her triggerin icinde fonksiyon olmasina gerek yok.
+Triggers tablo uzerinde olur objedir.
+
+
+
+--Employees yedek tablosuna delete audit ekleme islemi (old)
 
 
 --Tablo yaratma
@@ -24,20 +29,40 @@ old_data text
 select * from employees_audit
 
 
--- Before Delete fonksiyon ile create information tablo ekle
+	-- Before Delete fonksiyon ile create information tablo ekle
 
-create or replace function log_deletion_audit()
-returns trigger as 
-$$ 
-begin 
-	insert into employees_audit(action,employee_id,deleted_at,old_data)
-	values ('SILINDI',old.employee_id,now(),row_to_json(old)::text);             --json :veriyi dosya tutma tipi
-	return old;
-end
-$$
-language plpqsqlS;
+	create or replace function log_deletion_audit()
+	returns trigger as 
+	$$ 
+	begin 
+		insert into employees_audit(action,employee_id,deleted_at,old_data)
+		values ('SILINDI',old.employee_id,now(),row_to_json(old)::text);             --json :veriyi dosya tutma tipi
+		return old;
+	end
+	$$
+	language plpqsqlS;
 
---After trigger ile event haline donusturme
+
+	--After trigger ile event haline donusturme
+	--Her satir icin islem silindigi zaman islemi yap trigger.
+	create trigger silme_audit
+		after delete on emp_yedek
+		for each  row 
+		execute function log_deletion_audit()
+
+
+
+	delete from emp_yedek where employee_id = 100  --silme 
+	select * from employees_audit  
+	delete from emp_yedek where employee_id < 200
+
+	explain analyse 
+		delete from emp_yedek where employee_id < 200
+
+	--Yedek Tablo yaratma
+	select * from employees
+	create table emp_yedek as select * from employees e
+	select * from emp_yedek
 
 
 
---
+++ Bootcamp SQL/Lessons/10.1.sql
@@ -0,0 +1,31 @@
+--12.07.2023 
+
+--Triggerlar (Tetikleme) -2
+
+
+--Insert Notifikasyon Trigger (New)
+
+
+create or replace function  insert notifikasyon()
+returns trigger as 
+$$
+begin
+raise notice 'Yeni bir kayit eklendi,employee ID %',new employee_id
+return new
+end 
+$$
+language plpqsqlS;
+
+
+--trigger ile iliskilendirme
+
+create trigger insert_notifikasyon_trigger
+after insert on emp_yedek
+for each row -- her satir icin
+execute function insert_notifikasyon()
+
+
+
+insert into emp_yedek  (first_name,last_name) values ('Ali','Veli')
+insert into emp_yedek  (employee_id,first_name,last_name) values ('230','Ali','Veli')
+insert into emp_yedek  (employee_id,first_name,last_name) values ('245','Ali','Veli')
